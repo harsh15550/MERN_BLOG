b4451eeff569324acaee3f3011347e5adcb979b8
Harsh Patel <hp877257@gmail.com>
2024-07-30 18:47:08 +0530

    fix

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -48,7 +48,12 @@ const login = async (req, res) => {
         const info = await bcrypt.compare(password, userData.password);
         if (info === true) {
             const token = jwt.sign({userId : userData._id , username} , process.env.JWT_SECRET)
-            res.cookie('token', token);
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false, // Set to false for HTTP
+                sameSite: 'Lax', // Set SameSite to 'Lax'
+                maxAge: 24 * 60 * 60 * 1000 // 1 day in milliseconds
+              });
             res.status(200).json({ success: true , userData, token, message: "Login successful" });
         } else {
             return res.status(200).json({ success: false, message: "Invalid password" });
